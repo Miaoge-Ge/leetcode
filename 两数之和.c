c83fbdfdb9d1938b59f2eee73b3d32f66a672e40
蒋淼 <124609700+Miaoge-Ge@users.noreply.github.com>
2023-04-07 21:01:41 +0800

    Update 两数之和.c

--- 两数之和.c
+++ 两数之和.c
@@ -1,8 +1,11 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    int *array=(int*)malloc(sizeof(int)*2);//申请内存空间
     for(int i=0;i<numsSize;i++){
         for(int j=i+1;j<numsSize;j++){
             if(target==nums[i]+nums[j]){
+                int *array=(int *)malloc(sizeof(int)*2);
                 array[0]=i;
                 array[1]=j;
                 *returnSize=2;
@@ -10,5 +13,6 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
             }
         }
     }
-    return;
+    *returnSize=0;
+    return NULL;
 }
